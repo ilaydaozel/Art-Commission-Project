fdcee244497754834b6bf7c5cc6ff58b517c5f64
ilaydaozel <ilaydaoozel@gmail.com>
2022-01-20 23:00:17 +0300

    hack deploy

--- take_order.php
+++ take_order.php
@@ -28,7 +28,8 @@ $password= '551cfc91';
 
 $con = mysqli_connect($host, $username, $password) or die ("Couldn't open connection");
 mysqli_select_db( $con, "heroku_a4c26417a470e78" );
-mysqli_query( $con,"INSERT INTO order_information (art_medium, photo, paper_size, person_amount, comment, price, customer_id, customer_name, customer_surname, customer_address, customer_email ) ". "VALUES ('$art_medium', '$photo' , '$paper_size', '$person_amount', '$comment', '$price', '$customer_id', '$customer_name','$customer_surname','$customer_address','$customer_email')");
+$clean_comment=mysqli_real_escape_string($con, $comment);
+mysqli_query( $con,"INSERT INTO order_information (art_medium, photo, paper_size, person_amount, comment, price, customer_id, customer_name, customer_surname, customer_address, customer_email ) ". "VALUES ('$art_medium', '$photo' , '$paper_size', '$person_amount', '$clean_comment', '$price', '$customer_id', '$customer_name','$customer_surname','$customer_address','$customer_email')");
 $handle=fopen("customer_orders.txt", "a");
 $order_line="Customer id:".$customer_id.", Customer name:".$customer_name.", Customer surname:".$customer_surname.", Customer address:".$customer_address.", Customer email:".$customer_email.", Medium:".$art_medium.", Photo:".$photo.", Paper size:".$paper_size.", Person amount:".$person_amount.", Comment:".$comment.", Price:".$price."\n";
 fwrite($handle, $order_line);
