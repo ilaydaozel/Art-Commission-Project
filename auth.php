656cfc27106dac8f14899a8f7bec2dbae5469eaa
ilaydaozel <ilaydaoozel@gmail.com>
2022-01-20 20:50:06 +0300

    third deploy

--- auth.php
+++ auth.php
@@ -7,12 +7,9 @@ $username= 'b893d69c34f150';
 $password= '551cfc91';
 
 $con = mysqli_connect($host, $username, $password) or die ("Couldn't open connection");
-mysqli_select_db( $con, "heroku_a4c26417a470e78" );
-
+mysqli_select_db( $con, 'heroku_a4c26417a470e78' )or die ("Couldn't open database");
 $user_check= mysqli_query($con, "SELECT * FROM information WHERE email='$email' and password='$password'");
 
-echo mysqli_num_rows($user_check);
-echo "aaa";
 if (isset($_POST["login"])){
 	if ((isset($_POST["email"])) && (isset($_POST["password"]) ) && mysqli_num_rows($user_check)){
 		$user_record = mysqli_fetch_row( $user_check );
